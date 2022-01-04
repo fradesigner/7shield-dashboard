476b56aae56124ba1e6e47a2119cb0ec6d00d19a
fradesigner <fradesigner@gmail.com>
2022-01-04 17:01:30 +0100

    Update Title 7Shield

--- src/core/server/rendering/views/template.tsx
+++ src/core/server/rendering/views/template.tsx
@@ -35,7 +35,7 @@ export const Template: FunctionComponent<Props> = ({
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width" />
-        <title>Elastic</title>
+        <title>7Shield Dashboard</title>
         <Fonts url={uiPublicUrl} />
         {/* The alternate icon is a fallback for Safari which does not yet support SVG favicons */}
         <link rel="alternate icon" type="image/png" href={`${uiPublicUrl}/favicons/favicon.png`} />
